676cf20f301b9a9e5b33603b5754c8f7b7ea0418
Nick Tabick <nicktabick@gmail.com>
2014-03-19 12:33:14 -0400

    Add new Facebook Ads ID, match Last Modified stamp style from EasyList

--- commithook-updatetimestamp.sh
+++ commithook-updatetimestamp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i "s/\!\ Last\ modified\:.*/\!\ Last\ modified\:\ $(date -u '+%d-%b-%Y %H:%M %Z')/" nt-adblock.txt
+sed -i "s/\!\ Last\ modified\:.*/\!\ Last\ modified\:\ $(date -u '+%d %b %Y %H:%M %Z')/" nt-adblock.txt
